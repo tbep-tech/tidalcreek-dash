c68b04c8757aaccc7a198da158207e36d99cb882
fawda123 <mbafs2012@gmail.com>
2020-03-05 16:21:18 -0500

    sarasota creeks added, bnds data object added

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,6 +1,20 @@
 library(tbeptools)
+library(tidyverse)
 library(here)
+
+# tidal creek results -----------------------------------------------------
 
 tidres <- anlz_tdlcrk(tidalcreeks, iwrraw)
 
-save(tidres, file = here('data', 'tidres.RData'), compress = 'xz')
+save(tidres, file = here::here('data', 'tidres.RData'), compress = 'xz')
+
+# watersheds for spatial filter -------------------------------------------
+
+# anep boundaries
+load(file = '../anep-congressional/data/anep.RData')
+
+# tbep and sbep
+bnds <- anep %>% 
+  filter(nep %in% c('Sarasota Bay Estuary Program', 'Tampa Bay Estuary Program'))
+
+save(bnds, file = here::here('data', 'bnds.RData'), compress = 'xz')
