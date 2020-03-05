d34ee9c7aed686363898e2421c0b2685d58fba7e
fawda123 <mbafs2012@gmail.com>
2020-03-05 08:36:03 -0500

    added threshold line selector to individual creek plots

--- R/funcs.R
+++ R/funcs.R
@@ -74,22 +74,65 @@ sumfun <- function(mapsel, sumsel, typsel, filt){
   
 }
 
-# add horizontal line to plotly
+# add horizontal or vertical line to plotly
 # https://stackoverflow.com/questions/55170349/adding-a-horizontal-line-to-a-plotly-bar-graph
-hline <- function(y = 0, color = "black") {
-  list(
-    type = "line", 
-    x0 = 0, 
-    x1 = 1, 
-    xref = "paper",
-    y0 = y, 
-    y1 = y, 
-    line = list(color = color, dash = 10)
-  )
+plyline <- function(ln = 0, thrsel = FALSE, horiz = T) {
+  
+  out <- NULL
+  
+  # create lines
+  if(thrsel){
+    
+    out <- list()
+    
+    # horizontal
+    if(horiz){
+      for(i in seq_along(ln)){
+
+        outi <- list(list(
+          type = "line", 
+          x0 = 0, 
+          x1 = 1, 
+          xref = "paper",
+          y0 = ln[i], 
+          y1 = ln[i], 
+          line = list(color = 'black', dash = 10)
+        ))
+        
+        out <- c(out, outi)
+        
+      }
+
+    }
+    
+    # vertical
+    if(!horiz){
+      for(i in seq_along(ln)){
+        
+        outi <- list(list(
+          type = "line", 
+          x0 = ln[i], 
+          x1 = ln[i], 
+          yref = "paper",
+          y0 = 0, 
+          y1 = 1, 
+          line = list(color = 'black', dash = 10)
+        ))
+        
+        out <- c(out, outi)
+        
+      }
+      
+    }
+    
+  }
+      
+  return(out)
+
 }
 
 # bar plots of tidal creek context indicators
-show_tdlcrkindic <- function(selcrk, cntdat, yr){
+show_tdlcrkindic <- function(selcrk, cntdat, yr, thrsel = FALSE){
   
   labs <- c('Chla (ug/L)', 'TN (mg/L)', 'Chla:TN', 'DO (mg/L)', 'Florida TSI')
   names(labs) <- c('CHLAC', 'TN', 'chla_tn_ratio', 'DO', 'tsi')
@@ -126,7 +169,7 @@ show_tdlcrkindic <- function(selcrk, cntdat, yr){
       yaxis = list(title = labs['CHLAC']), 
       xaxis = list(title = ''), 
       showlegend = F, 
-      shapes = list(hline(thrs[['CHLAC']]))
+      shapes = plyline(thrs[['CHLAC']], thrsel = thrsel)
     )
   
   p2 <- plot_ly(toplo, x = ~year, y = ~TN, type = 'bar', text = ~round(TN, 1), textposition = 'auto', 
@@ -136,7 +179,7 @@ show_tdlcrkindic <- function(selcrk, cntdat, yr){
       yaxis = list(title = labs['TN']), 
       xaxis = list(title = ''), 
       showlegend = F, 
-      shapes = list(hline(thrs[['TN']]))
+      shapes = plyline(thrs[['TN']], thrsel = thrsel)
     )
   
   p3 <- plot_ly(toplo, x = ~year, y = ~chla_tn_ratio, type = 'bar', text = ~round(chla_tn_ratio, 1), textposition = 'auto', 
@@ -146,7 +189,7 @@ show_tdlcrkindic <- function(selcrk, cntdat, yr){
       yaxis = list(title = labs['chla_tn_ratio']), 
       xaxis = list(title = ''), 
       showlegend = F, 
-      shapes = list(hline(thrs[['chla_tn_ratio']]))
+      shapes = plyline(thrs[['chla_tn_ratio']], thrsel = thrsel)
     )
   
   p4 <- plot_ly(toplo, x = ~year, y = ~DO, type = 'bar', text = ~round(DO, 1), textposition = 'auto', 
@@ -155,7 +198,8 @@ show_tdlcrkindic <- function(selcrk, cntdat, yr){
     layout(
       yaxis = list(title = labs['DO']), 
       xaxis = list(title = ''), 
-      showlegend = F
+      showlegend = F,, 
+      shapes = plyline(thrs[['DO']], thrsel = thrsel)
     )
   
   p5 <- plot_ly(toplo, x = ~year, y = ~tsi, type = 'bar', text = ~round(tsi, 0), textposition = 'auto', 
@@ -165,7 +209,7 @@ show_tdlcrkindic <- function(selcrk, cntdat, yr){
       yaxis = list(title = labs['tsi']), 
       xaxis = list(title = ''), 
       showlegend = F, 
-      shapes = list(hline(thrs[['tsi']][1]),hline(thrs[['tsi']][2]))
+      shapes = plyline(thrs[['tsi']], thrsel = thrsel)
     )
   
   out <- subplot(p1, p2, p3, p4, p5, shareX = T, titleY = T, nrows = 5)
@@ -175,13 +219,22 @@ show_tdlcrkindic <- function(selcrk, cntdat, yr){
   }
 
 # ecdf plots of tidal creek context indicators
-show_tdlcrkindiccdf <- function(selcrk, cntdat, yr){
+show_tdlcrkindiccdf <- function(selcrk, cntdat, yr, thrsel = FALSE){
 
   # data to plot
   seldat <- cntdat %>% 
     filter(id %in% selcrk) %>% 
     mutate(year = factor(year, levels = seq(yr - 10, yr - 1))) %>% 
     tidyr::complete(id, wbid, JEI, class, year) 
+  
+  # thresholds
+  thrs <- list(
+    CHLAC = 11, 
+    TN = 1.1, 
+    chla_tn_ratio = 15, 
+    DO = 2, 
+    tsi = c(50, 60) # lake, estuary
+  )
   
   if(nrow(seldat) == 0)
     return()
@@ -225,7 +278,8 @@ show_tdlcrkindiccdf <- function(selcrk, cntdat, yr){
                     hoverinfo = 'text', text = ~year, showlegend = F) %>% 
           layout(
             yaxis = list(title = 'Percentiles', zeroline = T),
-            xaxis = list(title = labs[var], zeroline = T)
+            xaxis = list(title = labs[var], zeroline = T),
+            shapes = plyline(thrs[[var]], thrsel = thrsel, horiz = F)
           )
         
         return(p)
