eec4d3f6a381c2680aae9171188b68c0d669e00c
fawda123 <mbafs2012@gmail.com>
2021-07-01 13:54:26 -0400

    added radar plot functionality

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -18,3 +18,19 @@ bnds <- anep %>%
   filter(nep %in% c('Sarasota Bay Estuary Program', 'Tampa Bay Estuary Program', 'Charlotte Harbor National Estuary Program'))
 
 save(bnds, file = here::here('data', 'bnds.RData'), compress = 'xz')
+
+# context indicator data --------------------------------------------------
+
+# context data
+cntdat <- anlz_tdlcrkindic(tidalcreeks, iwrraw, yr = 2018) %>% 
+  select(id, wbid, JEI, class, year, CHLAC, DO, TN, chla_tn_ratio, tsi, no23_source, no23_tidal, no23_ratio)
+
+save(cntdat, file = here::here('data', 'cntdat.RData'), version = 2)
+
+# context indicator data, radar plot --------------------------------------
+
+# context data, radar plot
+cntdatrdr <- anlz_tdlcrkindic(tidalcreeks, iwrraw, yr = 2018, radar = T)
+
+save(cntdatrdr, file = here::here('data', 'cntdatrdr.RData'), version = 2)
+
