b478ed8bdde8367bd14a2753983041eded283aa2
fawda123 <mbafs2012@gmail.com>
2021-07-12 10:46:04 -0400

    reproc data with iwr run 61 and tbeptools update

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,12 +2,6 @@ library(tbeptools)
 library(tidyverse)
 library(here)
 
-# tidal creek results -----------------------------------------------------
-
-tidres <- anlz_tdlcrk(tidalcreeks, iwrraw)
-
-save(tidres, file = here::here('data', 'tidres.RData'), compress = 'xz')
-
 # watersheds for spatial filter -------------------------------------------
 
 # anep boundaries
@@ -19,10 +13,16 @@ bnds <- anep %>%
 
 save(bnds, file = here::here('data', 'bnds.RData'), compress = 'xz')
 
+# tidal creek results -----------------------------------------------------
+
+tidres <- anlz_tdlcrk(tidalcreeks, iwrraw)
+
+save(tidres, file = here::here('data', 'tidres.RData'), compress = 'xz')
+
 # context indicator data --------------------------------------------------
 
 # context data
-cntdat <- anlz_tdlcrkindic(tidalcreeks, iwrraw, yr = 2018) %>% 
+cntdat <- anlz_tdlcrkindic(tidalcreeks, iwrraw) %>% 
   select(id, wbid, JEI, class, year, CHLAC, DO, TN, chla_tn_ratio, tsi, no23_source, no23_tidal, no23_ratio)
 
 save(cntdat, file = here::here('data', 'cntdat.RData'), version = 2)
@@ -30,7 +30,7 @@ save(cntdat, file = here::here('data', 'cntdat.RData'), version = 2)
 # context indicator data, radar plot --------------------------------------
 
 # context data, radar plot
-cntdatrdr <- anlz_tdlcrkindic(tidalcreeks, iwrraw, yr = 2018, radar = T)
+cntdatrdr <- anlz_tdlcrkindic(tidalcreeks, iwrraw, radar = T)
 
 save(cntdatrdr, file = here::here('data', 'cntdatrdr.RData'), version = 2)
 
