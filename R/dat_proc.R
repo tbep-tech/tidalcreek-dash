b2a2b9f15a938b2b3ccee8b6568d936486616fc2
fawda123 <mbafs2012@gmail.com>
2021-07-13 07:37:12 -0400

    no more bnds spatial filter for creeks layer, removed bnds data

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,17 +2,6 @@ library(tbeptools)
 library(tidyverse)
 library(here)
 
-# watersheds for spatial filter -------------------------------------------
-
-# anep boundaries
-load(file = '../anep-congressional/data/anep.RData')
-
-# tbep and sbep
-bnds <- anep %>% 
-  filter(nep %in% c('Sarasota Bay Estuary Program', 'Tampa Bay Estuary Program', 'Charlotte Harbor National Estuary Program'))
-
-save(bnds, file = here::here('data', 'bnds.RData'), compress = 'xz')
-
 # tidal creek results -----------------------------------------------------
 
 tidres <- anlz_tdlcrk(tidalcreeks, iwrraw)
