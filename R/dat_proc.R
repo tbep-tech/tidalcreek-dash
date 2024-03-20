86529444a78b3e2e6b9f2245114d5dff19ee2b9c
fawda123 <mbafs2012@gmail.com>
2024-03-20 09:30:40 -0400

    update for iwr 65 2023 update

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -29,7 +29,7 @@ save(cntdatrdr, file = here::here('data', 'cntdatrdr.RData'), version = 2)
 library(sf)
 library(mapview)
 
-# run64 WBIDs
+# run65 WBIDs
 wbidraw <- st_read('https://ca.dep.state.fl.us/arcgis/rest/services/OpenData/WBIDS/MapServer/0/query?outFields=*&where=1%3D1&f=geojson')
 
 wbid <- wbidraw %>% 
